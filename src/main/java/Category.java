dbb43542ff79a69c82a2bb653ed8760559aa4aeb
Emin Şahin Oğuz Vurgun Mektepli <128241291+Sahin-Mektepli@users.noreply.github.com>
2023-12-01 18:00:56 +0300

    şunu artı pushlamış olayım. Emin'in yazdığı listCategories ile İpek'in getCategories tamamen aynı... Küçükm şapşiklikler

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,3 +1,9 @@
+import javax.xml.stream.events.StartElement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class Category {
     public final int categoryId;
     public final String name;
@@ -13,5 +19,39 @@ public class Category {
 
     public String getName() {
         return name;
+    }
+
+    public static ArrayList<Category> listCategories(){
+        ArrayList<Category> categories = new ArrayList<>();
+        try {
+            Statement stmt = DBConnection.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery("select * from category");
+            while(rs.next()){
+                categories.add(new Category(rs.getInt(2), rs.getString(1))); //we had it backwars, sorry lads
+            }
+        } catch(SQLException e){ e.printStackTrace();}
+        return categories;
+    }
+
+
+    public static ArrayList<Category> getCategories(){
+        ArrayList<Category> category=new ArrayList<>();
+        try{
+            Statement stmt = DBConnection.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery("select * from category");
+            while(rs.next()){
+                Category c=new Category(rs.getInt(1),rs.getString(2));
+                category.add(c);
+            }
+        } catch(SQLException e){e.printStackTrace();}
+        return category;
+    }
+
+    private static boolean categoryDoesntPreexist(Category category){
+        for(Category cat : listCategories()){
+            if(category.getName().equals(cat.getName()))
+                return false;
+        }
+        return true;
     }
 }
