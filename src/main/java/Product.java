d9af522bd92aaf4179a2ac84cfed9bbfdfbd0b16
ipekm <ipek.motorcu@ozu.edu.tr>
2023-12-01 19:17:52 +0300

    removeProduct metodunu yazdım

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,6 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Product {
     public final int productId;
     public final String name;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public Product(int productId, String name) {
         this.productId = productId;
@@ -13,5 +22,34 @@ public class Product {
 
     public String getName() {
         return name;
+    }
+
+
+    public static boolean removeProduct(){
+        boolean s = false;
+
+        try{
+            System.out.println("Enter Product ID: ");
+            int id= Integer.parseInt(br.readLine());
+
+            Statement  stmt = DBConnection.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery("select id from product where id="+id);
+                if (rs.next()){//istenilen product mevcutsa tabloda
+
+                    stmt.executeUpdate("delete from listing where product_id = "+id);//bu sanırım gereksiz ama şimdilik kalsın sağlam kafayla test edip ona göre sileriz
+                    stmt.executeUpdate("delete from  product where id="+id);
+
+                    s=true;
+                }
+
+
+
+
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    return s;
     }
 }
